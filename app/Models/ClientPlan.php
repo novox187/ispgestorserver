9373cab72bf7624d4b3796ecf32c56dd9babeb79
Harrison <novox187@gmail.com>
2025-12-19 08:30:37 +0300

    Refactorizar (ClientPlan): cambiar la clase base de Pivote a Modelo.
    Este cambio se realizó para alinearse mejor con el uso actual de ClientPlan como modelo regular, en lugar de como una relación de tabla dinámica.

--- app/Models/ClientPlan.php
+++ app/Models/ClientPlan.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Traits\Auditable;
 
-class ClientPlan extends Pivot
+class ClientPlan extends Model
 {
     use HasFactory, Auditable;
 
